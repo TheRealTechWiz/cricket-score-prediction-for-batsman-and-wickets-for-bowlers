3f9ae58ceabfd7d613d54837efbf0f6aefa1f429
TheRealTechWiz <w3.goyal@gmail.com>
2020-10-16 15:50:54 +0530

    add basic android project skeleton

---
+++ app/src/main/java/com/example/cricket_score_prediction_for_batsman_and_wickets_for_bowlers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cricket_score_prediction_for_batsman_and_wickets_for_bowlers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
